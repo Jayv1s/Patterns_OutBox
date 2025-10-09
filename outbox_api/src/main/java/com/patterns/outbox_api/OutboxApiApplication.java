0f8ba9a1ed447f74e313a3abf59faf1e40f91714
Jayv1s <joaovsgcontato@gmail.com>
2025-10-08 22:17:35 -0300

    feature: add API service

---
+++ outbox_api/src/main/java/com/patterns/outbox_api/OutboxApiApplication.java
@@ -0,0 +1,13 @@
+package com.patterns.outbox_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutboxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutboxApiApplication.class, args);
+	}
+
+}
